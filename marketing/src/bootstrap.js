6ee26d63245fae5974250593e0489f1dbd9c269e
Hanna Söderström <gothbarbie84@gmail.com>
2022-02-20 15:56:54 +0100

    feat: add marketing client

---
+++ marketing/src/bootstrap.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+const mount = (el) => {
+  ReactDOM.render(<App />, el);
+};
+
+if (process.env.NODE_ENV === "development") {
+  const devRootEl = document.querySelector("#_marketing-dev-root");
+
+  if (devRootEl) {
+    mount(devRootEl);
+  }
+}
+
+export { mount };
