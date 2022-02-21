a5175cf506cef091eda460d55d75ce3119054295
Hanna Söderström <gothbarbie84@gmail.com>
2022-02-21 10:05:15 +0100

    feat: manage browser history

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -1,11 +1,23 @@
 import React, { useRef, useEffect } from "react";
 import { mount } from "marketing/Marketing";
+import { useHistory } from "react-router-dom";
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory(); // Browser history
 
   useEffect(() => {
-    mount(ref.current);
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location;
+
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
+
+    history.listen(onParentNavigate);
   }, []);
 
   return <div ref={ref} />;
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,16 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+// Use browserHistory for development environment
+// Otherwise, share memoryHistory between micro frontend clients
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 if (process.env.NODE_ENV === "development") {
   const devRootEl = document.querySelector("#_marketing-dev-root");
 
   if (devRootEl) {
-    mount(devRootEl);
+    mount(devRootEl, { defaultHistory: createBrowserHistory() });
   }
 }
 
